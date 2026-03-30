80ab8fb8c5dc0d456b73af72d95236dcf4527bf0
s28101 <s28101@pjwstk.edu.pl>
2026-03-30 19:32:08 +0200

    Added basic StorageService

---
+++ RentalServiceApp/Service/StorageService.cs
@@ -0,0 +1,31 @@
+﻿
+
+using RentalServiceApp.Entity.Abstract;
+using RentalServiceApp.Entity.Model;
+
+namespace RentalServiceApp.Service;
+
+public class StorageService
+{
+    public static Dictionary<long, User> Users { get; private set; } = new ();
+    public static Dictionary<long, Rental> Rentals { get; private set; } = new ();
+    public static Dictionary<long, Medium> Items { get; private set; } = new ();
+    
+    public static void addUser(User user)
+    {
+        Users.Add(user.Id, user);
+    }
+    
+    public static void addRental(Rental rental){
+        Rentals.Add(rental.Id, rental);
+    }
+
+    public static void addItem(Medium item)
+    {
+        Items.Add(item.Id, item);
+    }
+
+    
+    
+
+}
