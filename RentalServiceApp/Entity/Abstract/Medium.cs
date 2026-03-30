94ac3312b4d442c47f3e1a335dc5e8388fd610d2
s28101 <s28101@pjwstk.edu.pl>
2026-03-30 21:28:22 +0200

    Added functionality to Medium to make an item unavailable without renting it

--- RentalServiceApp/Entity/Abstract/Medium.cs
+++ RentalServiceApp/Entity/Abstract/Medium.cs
@@ -7,12 +7,39 @@ public abstract class Medium
     public string Title { get; set; }
     public string Description { get; set; }
     public bool IsAvailable { get; set; }
+    public bool IsRentable { get; set; } = true;
 
     protected Medium(string title, string description, bool isAvailable)
     {
         Title = title;
         Description = description;
         IsAvailable = isAvailable;
+    }
+
+    public bool takeForMaintainace()
+    {
+        if (!IsAvailable)
+        {
+            Console.WriteLine("Item isn't available");
+            return false;
+        }
+        
+        IsAvailable = false;
+        IsRentable = false;
+        return true;
+    }
+
+    public bool returnItemFromMaintainace()
+    {
+        if (IsRentable)
+        {
+            Console.WriteLine("Item wasn't taken for maintenance");
+            return false;
+        }
+        
+        IsAvailable = true;
+        IsRentable = true;
+        return true;
     }
 
     public override string ToString()
