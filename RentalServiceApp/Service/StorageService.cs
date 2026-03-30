f743e2969b140473cb0753bed9021f77accf2c8b
s28101 <s28101@pjwstk.edu.pl>
2026-03-30 21:16:08 +0200

    Added more basic filters

--- RentalServiceApp/Service/StorageService.cs
+++ RentalServiceApp/Service/StorageService.cs
@@ -16,15 +16,40 @@ public class StorageService
         Users.Add(user.Id, user);
     }
     
+    public List<Student> GetStudents()
+      {
+          List<Student> result = new List<Student>();
+  
+          foreach (User user in Users.Values)
+          {
+              if (user is Student student)
+              {
+                  result.Add(student);
+              }
+          }
+          
+          return  result;
+      }
+  
+    public List<Employee> GetEmployees()
+      {
+          List<Employee> result = new List<Employee>();
+          
+          foreach(User user in Users.Values)
+          {
+              if (user is Employee employee)
+              {
+                  result.Add(employee);
+              }
+          }
+          
+          return result;
+      }
+  
     public static void addRental(Rental rental){
         Rentals.Add(rental.Id, rental);
     }
-
-    public static void addItem(Medium item)
-    {
-        Items.Add(item.Id, item);
-    }
-
+    
     private List<Rental> getRentalsFiltered(Predicate<Rental> predicate)
     {
         var result = new List<Rental>();
@@ -75,36 +100,52 @@ public class StorageService
         return getRentalsFiltered(filter);
     }
 
-    public List<Student> GetStudents()
+    public List<Rental> getUsersRentals(User user)
     {
-        List<Student> result = new List<Student>();
-
-        foreach (User user in Users.Values)
-        {
-            if (user is Student student)
-            {
-                result.Add(student);
-            }
-        }
+        Predicate<Rental> filter = rental => rental.Reciever == user;
         
-        return  result;
+        return getRentalsFiltered(filter);
     }
 
-    public List<Employee> GetEmployees()
+    public List<Rental> getItemsRentals(Medium item)
     {
-        List<Employee> result = new List<Employee>();
-        
-        foreach(User user in Users.Values)
+        Predicate<Rental> filter = rental => rental.Item == item;
+        return getRentalsFiltered(filter);
+    }
+    
+    public static void addItem(Medium item)
         {
-            if (user is Employee employee)
+            Items.Add(item.Id, item);
+        }
+
+    private List<Medium> getItemsFiltered(Predicate<Medium> predicate)
+    {
+        var result = new List<Medium>();
+
+        foreach (Medium item in Items.Values)
+        {
+            if (predicate(item))
             {
-                result.Add(employee);
+                result.Add(item);
             }
         }
-        
+
         return result;
     }
 
+    public List<Medium> getAvailableItems()
+    {
+        Predicate<Medium> filter = item => item.IsAvailable;
+        
+        return getItemsFiltered(filter);
+    }
+
+    public List<Medium> getNotAvailableItems()
+    {
+        Predicate<Medium> filter = item => !item.IsAvailable;
+        return getItemsFiltered(filter);
+    }
+    
     
 
 }
